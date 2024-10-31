98ad9feb239c0defae4bb01fcfac3e116e90402f
YuriViligiardi <yuriviligiardi01@gmail.com>
2024-10-31 11:40:46 +0200

    Aggiunta form a biglietti.html e funzioni script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeVisibility() {
+    let div = document.getElementById("divBiglietti");
+    if (div.style.display = "null") {
+        div.style.display = "block"; 
+    } else {
+        div.style.display = "null";
+    }
+}
+
