73bea930eed50c5fca116c00273a9461f7a37e17
YuriViligiardi <yuriviligiardi01@gmail.com>
2024-11-05 00:38:21 +0100

    Aggiunta cartella immagini, style.css e gestioneAcquisti.php

--- gestioneacquisto.php
+++ gestioneacquisto.php
@@ -9,7 +9,111 @@
 </head>
 <body>
     <?php
-    
+        $listCf = array();
+
+        $nome = $_GET["name"];
+        $cognome = $_GET["surname"];
+        $cf = $_GET["cf"];
+        $settore = $_GET["sector"];
+        $codiceSconto = $_GET["codDiscount"];
+        $listCfAgg = getCf($listCf);
+
+        $numPers = searchNumPers($listCfAgg);
+        
+        $cost = ticketCost($settore, $numPers);
+        $sconto = discount($codiceSconto, $cost);
+        $costTot = calculateCostTot($codiceSconto, $cost, $settore, $numPers);
+
+        $listData = array("Nome" => $nome, "Cognome" => $cognome, "Codice fiscale Acquirente" => $cf, "Data dell'acquisto" => date("Y/m/d"), "Orario dell'acquisto" => date("h:i:sa"), "ListCfAgg" => $listCfAgg, "Costo" => $cost, "Sconto" => $sconto, "Costo totale" => $costTot);
+
+        showData($listData);
+
+        function getCf($lCf){
+            for ($i=1; $i < 5; $i++) { 
+                $cfPers = "cfPers" . $i;
+                if (isset($_GET[$cfPers])) {
+                    $newCf = $_GET[$cfPers];
+                    $lCf[$i] = $newCf;
+                }
+            }
+            return $lCf;
+        }
+
+        function searchNumPers($lCfA){
+            $num = count($lCfA);
+            if($num == 0){
+                return 1;
+            } else {
+                return $num + 1;
+            }
+        }
+
+        function discount($cs, $c){
+            if ($cs == "FIRENZE5") {
+                return ($c / 100) * 5;
+            } else if (isset($cs)) {
+                return "Nessun codice inserito";
+            } else {
+                return "Codice inesistente";
+            }
+        }
+
+        function ticketCost($set, $numPers){
+           switch ($set) {
+            case "curva":
+                return 30 * $numPers;
+
+            case "tribuna centrale":
+                return 80 * $numPers;
+
+            case "tribuna d'onore":
+                return 120 * $numPers;
+            
+            default:
+                break;
+           }
+        }
+
+        function calculateCostTot($cs, $c, $set, $n){
+            $cost = ticketCost($set, $n);
+            $sconto = discount($cs, $c);
+            if ($sconto == "Codice inesistente") {
+                return $cost;
+            } elseif ($sconto == "Nessun codice inserito") {
+                return $cost;
+            } else {
+                return $cost - $sconto; 
+            }
+            
+        }
+
+        function showData($ld){
+            echo "<div id='showData'>";
+                echo "<h1>DATI DEL BIGLIETTO</h1>"; 
+                foreach ($ld as $key => $value) {
+                    if ($key == "ListCfAgg") {
+                        echo "<p><b><i>Lista codici fiscali persone Aggiunte</i></b></p>";
+                        // echo "<ul>";
+                            for ($i=1; $i <= count($value); $i++) { 
+                                //echo "<li>" . $value[$i] . "</li>";
+                                echo "<p>- " . $value[$i] . "</p>";
+                            }
+                            echo "<p> ... </p>";
+                        //echo "</ul>";
+                    } elseif ($key == "Costo" || $key == "Sconto" || $key == "Costo totale") {
+                        echo "<p>";
+                        echo "<span><b><i>$key: </i></b></span>"; 
+                        echo $value . " euro"; 
+                        echo "</p>";
+                    } else {
+                        echo "<p>";
+                        echo "<span><b><i>$key: </i></b></span>"; 
+                        echo $value; 
+                        echo "</p>";
+                    }
+                }
+            echo "</div>";
+        }
     ?>
 </body>
 </html>
--- script.js
+++ script.js
@@ -1,9 +1,75 @@
-function changeVisibility() {
+let mes = document.getElementById("message");
+let contenitoreCf = document.getElementById("contenitoreInputCf");
+let count = 1; 
+
+function changeVisibilityOn() {
     let div = document.getElementById("divBiglietti");
-    if (div.style.display = "null") {
+    if (div.style.display = "none") {
         div.style.display = "block"; 
     } else {
-        div.style.display = "null";
+        div.style.display = "none";
     }
 }
+
+function resetContenitoreCf() {
+    let numPers = document.getElementById("numPers");
+    for (let i = 2; i <= 4; i++) {
+        count = i;
+        deleteInput();
+    }
+    count = 1;
+    numPers.value = parseInt(count);
+}  
+
+function changeVisibilityOff() {
+    let div = document.getElementById("divBiglietti");
+    resetContenitoreCf();
+    if (div.style.display = "block") {
+        div.style.display = "none"; 
+    } else {
+        div.style.display = "block";
+    }
+}
+
+function increasePerson(){
+    let numPers = document.getElementById("numPers");
+    if (numPers.value < 4) {
+        numPers.value = parseInt(numPers.value) + 1;
+        count++;
+        createInput();
+    } else {
+        mes.style.cssText = "background-color: red; color: white; width: 50%; margin: auto;";
+        mes.innerHTML = "HAI RAGGIUNTO NUMERO MASSIMO DI PERSONE CHE PUOI AGGIUNGERE";
+    }
+}
+
+function decreasePerson(){
+    let numPers = document.getElementById("numPers");
+    if (numPers.value > 1) {
+        numPers.value = parseInt(numPers.value) - 1;
+        deleteInput();
+        count--;
+    } else {
+        mes.style.cssText = "background-color: green; color: white; width: 50%; margin: auto;";
+        mes.innerHTML = "HAI RAGGIUNTO NUMERO MINIMO DI PERSONE CHE PUOI AGGIUNGERE";
+    }
+}
+
+function createInput() {
+    let newInput = document.createElement("input");
+    newInput.type = "text";
+    newInput.name = "cfPers" + count;
+    newInput.id = "cfPers" + count;
+    newInput.placeholder = "Nuovo codice fiscale";
+
+    contenitoreCf.appendChild(newInput);
+}
+
+function deleteInput() {
+    let deleteInput = document.getElementById("cfPers" + count); 
+    if (deleteInput) {
+        contenitoreCf.removeChild(deleteInput);
+    }
+}
+
 
